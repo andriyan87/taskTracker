e3f38e57eb3127b7d366201654c40de175781ece
andriyan87 <andriyanatanasov@gmail.com>
2021-08-25 18:33:37 +0300

    add AddTask component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
   <Header title= "Task Tracker"  />
+  <AddTask/>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
 </div>
   
@@ -10,11 +11,13 @@
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask
   },
   data() {
     return {
